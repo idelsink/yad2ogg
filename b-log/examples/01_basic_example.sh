c46c95837a113f6803aaf987c515fa277df4c8e1
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-07-02 19:56:53 +0200

    add a bash logger

---
+++ b-log/examples/01_basic_example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# example: 01 - basic example
+source ${0%/*}/../b-log.sh  # include the script
+LOG_LEVEL_ALL               # set log level to all
+FATAL   "fatal level"
+ERROR   "error level"
+WARN    "warning level"
+NOTICE  "notice level"
+INFO    "info level"
+DEBUG   "debug level"
+TRACE   "trace level"
